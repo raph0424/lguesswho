b33073dce5a37d14bde0e88865b567ddd7905036
Charlie Haag <charlottehaag22@gmail.com>
2018-08-05 14:59:34 -0700

    shuffle myCharacter on gamereset

--- js/game.js
+++ js/game.js
@@ -3,20 +3,24 @@ $(window).on('load', function(){
   setTimeout(function(){
     $('.loader').fadeOut();
   }, 1500)
-
 })
 
-
+// Characters
 var pics = ["bellatrix.jpg", "dobby.jpg", "draco.jpg", "dudley.jpg", "dumbledore.jpg", "fred.png", "ginny.jpg", "hagrid.png", "harry.jpg", "hermione.jpg", "luna.jpeg", "lupin.jpg", "madeye.png", "mcgonagall.jpg", "molly.jpg", "arthur.jpg", "myrtle.jpg", "neville.jpg", "petunia.jpg", "ron.jpg", "snape.jpg", "vernon.jpg", "voldemort.jpg", "sirius.jpg", "fatlady.png"];
 
 //My Guess Who Character
-var myPic = Math.floor(Math.random() * pics.length);
+function myCharacter(){
+  var myPic = Math.floor(Math.random() * pics.length);
 
-$("#me").html('<img src="img/' + pics[myPic] + ' ">');
+  $("#me").html('<img src="img/' + pics[myPic] + ' ">');
 
-var name = pics[myPic].substr(0, pics[myPic].lastIndexOf("."))
-name = name.charAt(0).toUpperCase() + name.slice(1);
-$("#name").html('You are ' + name);
+  var name = pics[myPic].substr(0, pics[myPic].lastIndexOf("."))
+  name = name.charAt(0).toUpperCase() + name.slice(1);
+  $("#name").html('You are ' + name);
+}
+
+myCharacter();
+
 
 //Game Board
 function shuffle() {
@@ -61,6 +65,7 @@ function reset(){
   var play = prompt('Do you want to play again?');
   if (play.toLowerCase() == 'yes'){
     $('#gameboard').html('');
+    myCharacter();
     shuffle();
   } else {
     alert('Thanks for playing!');
